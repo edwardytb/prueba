1ed5aaa40053c0a941adc1d8cbd5eba1a68cec36
edwardytb <104452175+edwardytb@users.noreply.github.com>
2023-06-18 22:06:15 -0600

    Delete assets directory

--- assets/js/contac.js
+++
@@ -1,9 +0,0 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const slideInterval = setInterval(nextSlide, 2000); // Cambia de slide cada 3 segundos
-
-function nextSlide() {
-  slides[currentSlide].style.display = 'none';
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].style.display = 'block';
-}
